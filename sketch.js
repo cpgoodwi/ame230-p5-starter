029fd20e12b85d8a72d3d83a40b1a729f9ad5e91
cpgoodwi <cpgoodwi@asu.edu>
2023-01-17 09:46:32 -0700

    added grid lines

--- sketch.js
+++ sketch.js
@@ -8,13 +8,20 @@ function setup() {
 function draw() {
 	background(128);
 
+    strokeWeight(1);
+    stroke(100);
+    for (let grid = 0; grid < 600; grid += 50) {
+        line(0, grid, 600, grid);
+        line(grid, 0, grid, 600);
+    }
+
     strokeWeight(10);
     stroke(0);
     fill(255);
     rectMode(CENTER);
     rect(300, 200, 100, 100);
     
-    fill(255, 0, 0);
+    fill(255, 0, 0, 128);
     ellipse(300, 300, 200, 200);
     
     stroke(0, 128, 0);
@@ -35,4 +42,10 @@ function draw() {
         0, PI + 1,
         OPEN
     );
+
+    beginShape();
+    vertex(100, 400);
+    vertex(300, 480);
+    vertex(500, 400);
+    endShape();
 }
