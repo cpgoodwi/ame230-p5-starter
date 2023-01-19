fadc594a577f912a9cd5192c81b508cd906c5b3d
cpgoodwi <cpgoodwi@asu.edu>
2023-01-19 09:17:36 -0700

    defined clouds and puffs

--- sketch.js
+++ sketch.js
@@ -20,18 +20,26 @@ function draw() {
 }
 
 class Cloud {
-    constructor(density, width, height, puffVariance) {
+    constructor(x, y, width, height, density = 10, puffVariance = 5) {
+        this.x = x; // x coordinate of the center base of the cloud
+        this.y = y; // y coordinate of the center base of the cloud
+        this.width = width; // the width at the base of the cloud in terms of puffs
+        this.height = height; // the height to the top of the cloud in terms of puffs
         this.density = density; // the distance between puffs
-        this.width = width; // the width at the base of the cloud
-        this.height = height; // the height to the top of the cloud
-        this.puffVariance = 
+        this.puffVariance = puffVariance // the variation of the puffs around their center point
     }
 }
 
 class Puff {
-    constructor(maxDiameter, x, y) {
-        this.maxDiameter = maxDiameter;
-        this.x = x; // 
-        this.y = y;
+    constructor(x, y, maxDiameter = 5, minColor = 192, maxColor = 255) {
+        this.x = x; // x coordinate of the puff
+        this.y = y; // y coordinate of the puff
+        this.maxDiameter = maxDiameter; // max diameter of the changing puff
+        this.minColor = minColor; // minimum gray value of the puff
+        this.maxColor = maxColor; // maximum gray value of the puff
+    }
+
+    drawSelf() {
+
     }
 }
