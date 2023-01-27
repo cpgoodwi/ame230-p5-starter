31504eb62232a0c7bbfacf9fde7e285d849866f1
cpgoodwi <cpgoodwi@asu.edu>
2023-01-27 08:46:06 -0700

    conditional demo

--- sketch.js
+++ sketch.js
@@ -1,5 +1,13 @@
 // sketch.js
 
+// global variables
+let positionX = 300;
+let positionY = 300;
+let radius = 40;
+let velocityX = 10;
+let velocityY = 15;
+let gravity = 1;
+
 function setup() {
 	createCanvas(600, 600);
 
@@ -8,5 +16,68 @@ function setup() {
 function draw() {
 	background(128);
 
+    if (mouseX < 300 && mouseY < 300) {
+        fill("peachpuff");
+        noStroke();
+        rect(
+            0, 0,
+            300, 300
+        );
+    } else if (mouseX > 300 && mouseY < 300) {
+        fill("blue");
+        noStroke();
+        rect(
+            300, 0,
+            300, 300
+        );
+    } else if (mouseX > 300 && mouseY > 300) {
+        fill("chartreuse");
+        noStroke();
+        rect(
+            300, 300,
+            300, 300
+        );
+    } else {
+        fill("cornsilk");
+        noStroke();
+        rect(
+            0, 300,
+            300, 300
+        );
+    }
 
+    positionX += velocityX;
+    positionY += velocityY;
+
+    if (positionX + radius >= width || positionX - radius <= 0) {
+        velocityX *= -1;
+    }
+
+    if (positionY + radius >= height || positionY - radius <= 0) {
+        velocityY *= -1
+    }
+
+    stroke(0);
+    strokeWeight(3);
+    fill(255);
+    ellipse(
+        positionX, positionY,
+        radius * 2, radius * 2
+    );
+
+    velocityY += gravity;
 }
+
+/**
+ * CONDITIONALS
+ * 
+ * Expressions and Operators
+ * Operator - is a special symbol that you use to check, change, 
+ * Comparison operators
+ *  >
+ *  <
+ *  >=
+ *  <=
+ *  ==
+ *  !=
+ */
