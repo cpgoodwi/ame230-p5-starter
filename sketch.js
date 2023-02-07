33c6abe6c5cf215dd795eeace444d2216d8aa62f
cpgoodwi <cpgoodwi@asu.edu>
2023-02-07 10:15:47 -0700

    circle/spiral

--- sketch.js
+++ sketch.js
@@ -11,56 +11,32 @@ function setup() {
 function draw() {
 	background(128);
 
-    let x = 0;
-    let y = 0;
-    let size = 50;
-    let padding = 2;
+    let centerX = 300;
+    let centerY = 300;
+    let radius = 10;
 
-    let rowCount = 0;
-    while (y < height) {
-        x = 0;
-        let colCount = 0;
+    noFill();
+    // ellipse(
+    //     centerX, centerY,
+    //     radius * 2, radius * 2
+    // );
 
-        while (x < width) {
-
-            // draw ONE tile
-
-            let x1 = x + padding;
-            let y1 = y + padding;
-            let size1 = size - padding * 2;
-
-            stroke(0);
-            strokeWeight(1);
-            // noFill();
-
-            // fill(rowCount % 2 ? "red" : "white");
-            fill(
-                random(0, 255),
-                random(0, 255),
-                random(0, 255)
-            );
-            rect(
-                x1, y1,
-                size1, size1
-            );
-
-            // line(
-            //     x1, y1,
-            //     x1 + size1, y1 + size1
-            // );
-
-            // line(
-            //     x1 + size1, y1,
-            //     x1 , y1 + size1
-            // );
-
-            x += size;
-            colCount++;
-        }
-
-        y += size;
-        rowCount++;
+    beginShape();
+    let a = 0.0;
+    while (a < TWO_PI * 10) {
+        let x = cos(a) * radius + centerX;
+        let y = sin(a) * radius + centerY;
+        // rectMode(CENTER);
+        // fill("red");
+        // rect(
+        //     x, y,
+        //     8, 8
+        // );
+        vertex(x, y);
+        a += PI * 0.05;
+        radius += .5;
     }
+    endShape();
 
     if (stop)
         noLoop();
