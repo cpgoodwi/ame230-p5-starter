e44846eb065628fec8ddd498f8f4fd32cbc864be
CharleyIsCool <cpgoodwi@asu.edu>
2023-04-09 23:54:54 -0700

    hacker typer

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 // sketch.js
 
-let quine, flatQuine, charIterator, renderedText, textSizeValue;
+let quine, flat, charIterator, renderedText, codeSize;
 
 function preload() {
 	quine = loadStrings("sketch.js");
@@ -12,39 +12,41 @@ function setup() {
 	fill("chartreuse");
 	noStroke();
 
-	flatQuine = [], renderedText = [], textSizeValue = 13;
+	flat = [], renderedText = [], codeSize = 12;
 
-	textFont("Inconsolata"); // my programming font from google fonts api
-	textSize(textSizeValue);
+	textFont("Inconsolata"); // my programming font from google fonts
+	textSize(codeSize);
 
 	let y = 0;
 	for (const line of quine) {
 		let x = 0;
 		for (const char of line) {
-			flatQuine.push(new Character(x, y, char));
+			flat.push(new Character(x, y, char));
 			x += char === "\t" ? 4 * textWidth(char) : textWidth(char);
 		}
-		y += textSizeValue;
+		y += codeSize;
 	}
-	charIterator = iterator(0, flatQuine.length);
+	charIterator = iterator(0, flat.length);
 }
 
 function draw() {
 	background("#010112");
-	translate(0, textSizeValue);
-
-	let index = charIterator.next();
-	if (!index.done) {
-		renderedText.push(flatQuine[index.value]);
-	}
+	translate(0, codeSize);
 
 	renderedText.forEach(char => text(char.char, char.x, char.y));
 }
 
-// c̵o̵p̵i̵e̵d̵ borrowed from mdn web docs iterators and generators tutorial
+function keyPressed() {
+	let index = charIterator.next();
+	if (!index.done) {
+		renderedText.push(flat[index.value]);
+	}
+}
+
+// borrowed from mdn web docs iterators and generators tutorial
 function* iterator(start = 0, end = Infinity, step = 1) {
 	let count = 0;
-	for (let i = 0; i < end; i += step) {
+	for (let i = start; i < end; i += step) {
 		count++;
 		yield i;
 	}
