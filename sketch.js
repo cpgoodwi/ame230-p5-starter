9e5f01dd01daa200a03784e8ea936a53cbc510de
cpgoodwi <cpgoodwi@asu.edu>
2023-04-17 14:13:56 -0700

    loaded phoenix data

--- sketch.js
+++ sketch.js
@@ -1,8 +1,20 @@
 // sketch.js
 
+let table;
+
+let minDataValue;
+let maxDataValue;
+let minTimeValue;
+let maxTimeValue;
+
+function preload() {
+    table = loadTable("phoenix-az.csv", "csv", "header");
+}
+
 function setup() {
 	createCanvas(600, 600);
-
+    print(table);
+    print(min(table.rows.map(row => row.arr[1])));
 }
 
 function draw() {
