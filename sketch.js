ab508139ee5e48168b1024b9e92b974ed10af22b
CharleyIsCool <cpgoodwi@asu.edu>
2023-04-27 02:47:30 -0700

    working on sound player

--- sketch.js
+++ sketch.js
@@ -1,12 +1,31 @@
 // sketch.js
 
+let sound;
+
+function preload() {
+	sound = loadSound("metal-bar-falling-sound-effect.mp3");
+}
+
 function setup() {
 	createCanvas(600, 600);
+	print(sound.isLoaded() ? "sound ready" : "sound not ready");
 
+	textFont("monospace");
+	textSize(40);
 }
 
 function draw() {
 	background(128);
 
+	text(frameCount, 300, 300);
+}
 
+function keyPressed() {
+	if (key === " ") {
+		sound.play();
+	}
+}
+
+function canvasButton(x, y, text, action) {
+	
 }
