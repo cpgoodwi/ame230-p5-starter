e15a8a227bd0a162182a74ff0fd99724d9b29585
CharleyIsCool <cpgoodwi@asu.edu>
2023-04-27 22:11:44 -0700

    working on sound

--- sketch.js
+++ sketch.js
@@ -9,6 +9,7 @@ function preload() {
 function setup() {
 	createCanvas(600, 600);
 	print(sound.isLoaded() ? "sound ready" : "sound not ready");
+	print(sound.duration())
 
 	textFont("monospace");
 	textSize(40);
@@ -17,7 +18,10 @@ function setup() {
 function draw() {
 	background(128);
 
-	text(frameCount, 300, 300);
+	if (sound.isPlaying()) {
+		text(sound.currentTime(), 100, 100);
+		soundProgressBar(300, 300, sound.currentTime(), sound.duration())
+	}
 }
 
 function keyPressed() {
@@ -26,6 +30,24 @@ function keyPressed() {
 	}
 }
 
-function canvasButton(x, y, text, action) {
-	
+function soundProgressBar(x, y, time, duration) {
+	const bar = {
+		start: x - 150,
+		end: x + 150,
+		height: 10,
+		width: 300,
+	}
+
+	// draw bar background
+	fill("white");
+	stroke("black");
+	strokeWeight(1);
+	rect(bar.start, y, bar.width, bar.height);
+
+	// draw progress meter
+	bar.progress = map(time, 0, duration, bar.start + 5, bar.end - 5);
+
+	strokeWeight(5);
+	stroke("black");
+	line(bar.start + 5, y + bar.height / 2, bar.progress, y + bar.height / 2)
 }
