be077d03926e4be34c5a30cee096d7a22f6703d1
CharleyIsCool <cpgoodwi@asu.edu>
2023-05-23 16:01:47 -0700

    final assn

--- sketch.js
+++ sketch.js
@@ -1,41 +1,79 @@
 // sketch.js
 
-let sound;
+let metalBarSound;
+let metalBarImage;
+
+// p5.Elements
+let controlsContainer;
+let rateLabel;
+let rateSlider;
+let playPauseButton;
+let volumeSlider;
+let volumeLabel;
 
 function preload() {
-	sound = loadSound("metal-bar-falling-sound-effect.mp3");
+	metalBarSound = loadSound("metal-bar-falling-sound-effect.mp3");
+	metalBarImage = loadImage("metal-bar.jpg");
 }
 
 function setup() {
-	createCanvas(600, 600);
-	print(sound.isLoaded() ? "sound ready" : "sound not ready");
-	print(sound.duration())
+	createCanvas(600, 400);
+	print(metalBarSound.isLoaded() ? "sound ready" : "sound not ready");
 
+	// using elements to control the sound, styling is in CSS
+	controlsContainer = createDiv();
+	controlsContainer.id('soundControls');
+
+	rateLabel = createSpan("Rate");
+	rateLabel.parent("#soundControls");
+	rateLabel.style("text-align", "right");
+	rateSlider = createSlider(0, 200, 100);
+	rateSlider.parent("#soundControls");
+
+	playPauseButton = createButton("\u{23F5}");
+	playPauseButton.parent("#soundControls");
+	playPauseButton.mousePressed(soundPlayPause);
+
+	volumeSlider = createSlider(0, 100);
+	volumeSlider.parent("#soundControls");
+	volumeLabel = createSpan("Volume");
+	volumeLabel.parent("#soundControls");
+	volumeLabel.style("text-align", "left");
+
+	textAlign(CENTER);
 	textFont("monospace");
-	textSize(40);
+	textSize(30);
 }
 
 function draw() {
 	background(128);
 
-	if (sound.isPlaying()) {
-		text(sound.currentTime(), 100, 100);
-		soundProgressBar(300, 300, sound.currentTime(), sound.duration())
-	}
-}
+	const rate = rateSlider.value();
+	const volume = volumeSlider.value();
 
-function keyPressed() {
-	if (key === " ") {
-		sound.play();
+	image(metalBarImage, 100, 50, 400, 200);
+
+	if (metalBarSound.isPlaying()) {
+		playPauseButton.html("\u{23F8}");
+	} else {
+		playPauseButton.html("\u{23F5}");
 	}
+
+	soundProgressBar(300, 300, metalBarSound.currentTime(), metalBarSound.duration())
+
+	text(`${rate}%`, 200, 400);
+	metalBarSound.rate(map(rate, 0, 200, 0, 2.0));
+
+	text(`${volume}%`, 400, 400);
+	metalBarSound.setVolume(map(volume, 0, 100, 0, 1.0));
 }
 
 function soundProgressBar(x, y, time, duration) {
 	const bar = {
-		start: x - 150,
-		end: x + 150,
+		start: x - 200,
+		end: x + 200,
 		height: 10,
-		width: 300,
+		width: 400,
 	}
 
 	// draw bar background
@@ -49,5 +87,15 @@ function soundProgressBar(x, y, time, duration) {
 
 	strokeWeight(5);
 	stroke("black");
-	line(bar.start + 5, y + bar.height / 2, bar.progress, y + bar.height / 2)
+	line(bar.start + 5, y + bar.height / 2, bar.progress, y + bar.height / 2);
+}
+
+function soundPlayPause() {
+	print("play/pause");
+
+	if (metalBarSound.isPlaying()) {
+		metalBarSound.pause();
+	} else {
+		metalBarSound.play();
+	}
 }
